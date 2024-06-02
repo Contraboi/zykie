7ff049e888c1c466c0affc11ba64f1834ecc7c9e
Selmir Nedzibi <Contraboi@users.noreply.github.com>
2024-06-02 22:30:56 +0200

    feat: support optional get variable argument

--- src/lib/zykie.test.ts
+++ src/lib/zykie.test.ts
@@ -6,196 +6,190 @@ const currentLocale = "en";
 const fallbackLocale = "ba";
 
 const zykie = new Zykie({
-    locales,
-    currentLocale,
-    fallbackLocale,
+  locales,
+  currentLocale,
+  fallbackLocale,
 });
 
 const hello = zykie.create({
-    en: "Hello!",
-    de: "Hallo!",
-    ba: "Zdravo!",
-    fr: null,
+  en: "Hello!",
+  de: "Hallo!",
+  ba: "Zdravo!",
+  fr: null,
 });
 
 const greet = zykie.create({
-    en: "Hello var{name}, you work at var{company}",
-    de: "Hallo var{name}, Sie arbeiten bei var{company}",
-    ba: "Zdravo var{name}, vi radite kod var{company}",
-    fr: null,
+  en: "Hello var{name}, you work at var{company}",
+  de: "Hallo var{name}, Sie arbeiten bei var{company}",
+  ba: "Zdravo var{name}, vi radite kod var{company}",
+  fr: null,
 });
 
 const youHaveNDollars = zykie
-    .create({
-        en: "You have var{amount} dollars",
-        de: "Sie haben var{amount} Dollar",
-        ba: "Imate var{amount} dolara",
-        fr: null,
-    })
-    .variation(({ amount }) => parseInt(amount) === 1, {
-        en: "You have var{amount} dollar",
-        ba: "Imate var{amount} dolar",
-    })
-    .variation(
-        ({ amount }) => {
-            const number = parseInt(amount);
-            return number > 20 && number % 10 === 1;
-        },
-        {
-            ba: "Imate var{amount} dolar",
-        }
-    );
+  .create({
+    en: "You have var{amount} dollars",
+    de: "Sie haben var{amount} Dollar",
+    ba: "Imate var{amount} dolara",
+    fr: null,
+  })
+  .variation(({ amount }) => parseInt(amount) === 1, {
+    en: "You have var{amount} dollar",
+    ba: "Imate var{amount} dolar",
+  })
+  .variation(
+    ({ amount }) => {
+      const number = parseInt(amount);
+      return number > 20 && number % 10 === 1;
+    },
+    {
+      ba: "Imate var{amount} dolar",
+    },
+  );
 
 const variation = zykie
-    .create({
-        en: "default var{variation}",
-        de: "Standard var{variation}",
-        ba: "Osnovni var{variation}",
-        fr: null,
-    })
-    .variation(({ variation }) => variation === "first", {
-        en: "first var{variation}",
-        de: "erste var{variation}",
-        ba: "prvi var{variation}",
-    })
-    .variation(({ variation }) => variation === "second", {
-        en: "second var{variation}",
-        de: "zweite var{variation}",
-        ba: "drugi var{variation}",
-    });
+  .create({
+    en: "default var{variation}",
+    de: "Standard var{variation}",
+    ba: "Osnovni var{variation}",
+    fr: null,
+  })
+  .variation(({ variation }) => variation === "first", {
+    en: "first var{variation}",
+    de: "erste var{variation}",
+    ba: "prvi var{variation}",
+  })
+  .variation(({ variation }) => variation === "second", {
+    en: "second var{variation}",
+    de: "zweite var{variation}",
+    ba: "drugi var{variation}",
+  });
 
 const greetStrings = {
-    name: "Contra",
-    company: "Zykie",
+  name: "Contra",
+  company: "Zykie",
 };
 
 test("Check default translations", () => {
-    expect(hello.get({})).toBe("Hello!");
-    expect(
-        greet.get({ name: greetStrings.name, company: greetStrings.company })
-    ).toBe(`Hello ${greetStrings.name}, you work at ${greetStrings.company}`);
+  expect(hello.get({})).toBe("Hello!");
+  expect(
+    greet.get({ name: greetStrings.name, company: greetStrings.company }),
+  ).toBe(`Hello ${greetStrings.name}, you work at ${greetStrings.company}`);
 
-    expect(
-        youHaveNDollars.get({
-            amount: "1",
-        })
-    ).toBe(`You have 1 dollar`);
+  expect(
+    youHaveNDollars.get({
+      amount: "1",
+    }),
+  ).toBe(`You have 1 dollar`);
 });
 
 test("Check translations for de locale", () => {
-    zykie.changeLocale("de");
+  zykie.changeLocale("de");
 
-    expect(hello.get({})).toBe("Hallo!");
+  expect(hello.get({})).toBe("Hallo!");
 
-    expect(
-        greet.get({ name: greetStrings.name, company: greetStrings.company })
-    ).toBe(
-        `Hallo ${greetStrings.name}, Sie arbeiten bei ${greetStrings.company}`
-    );
+  expect(
+    greet.get({ name: greetStrings.name, company: greetStrings.company }),
+  ).toBe(
+    `Hallo ${greetStrings.name}, Sie arbeiten bei ${greetStrings.company}`,
+  );
 
-    expect(
-        youHaveNDollars.get({
-            amount: "1",
-        })
-    ).toBe(`Sie haben 1 Dollar`);
+  expect(
+    youHaveNDollars.get({
+      amount: "1",
+    }),
+  ).toBe(`Sie haben 1 Dollar`);
 });
 
 test("Check translations for ba locale", () => {
-    zykie.changeLocale("ba");
+  zykie.changeLocale("ba");
 
-    expect(hello.get({})).toBe("Zdravo!");
+  expect(hello.get({})).toBe("Zdravo!");
 
-    expect(
-        greet.get({ name: greetStrings.name, company: greetStrings.company })
-    ).toBe(
-        `Zdravo ${greetStrings.name}, vi radite kod ${greetStrings.company}`
-    );
+  expect(
+    greet.get({ name: greetStrings.name, company: greetStrings.company }),
+  ).toBe(`Zdravo ${greetStrings.name}, vi radite kod ${greetStrings.company}`);
 
-    expect(
-        youHaveNDollars.get({
-            amount: "1",
-        })
-    ).toBe(`Imate 1 dolar`);
+  expect(
+    youHaveNDollars.get({
+      amount: "1",
+    }),
+  ).toBe(`Imate 1 dolar`);
 });
 
 test("Check translations for fr locale with fallback locale", () => {
-    zykie.changeLocale("fr");
+  zykie.changeLocale("fr");
 
-    expect(hello.get({})).toBe("Zdravo!");
+  expect(hello.get({})).toBe("Zdravo!");
 
-    expect(
-        greet.get({
-            name: greetStrings.name,
-            company: greetStrings.company,
-        })
-    ).toBe(
-        `Zdravo ${greetStrings.name}, vi radite kod ${greetStrings.company}`
-    );
+  expect(
+    greet.get({
+      name: greetStrings.name,
+      company: greetStrings.company,
+    }),
+  ).toBe(`Zdravo ${greetStrings.name}, vi radite kod ${greetStrings.company}`);
 
-    expect(
-        youHaveNDollars.get({
-            amount: "1",
-        })
-    ).toBe(`Imate 1 dolar`);
+  expect(
+    youHaveNDollars.get({
+      amount: "1",
+    }),
+  ).toBe(`Imate 1 dolar`);
 });
 
 test("With overwritten locale from de to ba", () => {
-    zykie.changeLocale("de");
+  zykie.changeLocale("de");
 
-    expect(hello.get({}, { locale: "ba" })).toBe("Zdravo!");
+  expect(hello.get({ locale: "ba" })).toBe("Zdravo!");
 
-    expect(
-        greet.get(
-            { name: greetStrings.name, company: greetStrings.company },
-            {
-                locale: "ba",
-            }
-        )
-    ).toBe(
-        `Zdravo ${greetStrings.name}, vi radite kod ${greetStrings.company}`
-    );
+  expect(
+    greet.get(
+      { name: greetStrings.name, company: greetStrings.company },
+      {
+        locale: "ba",
+      },
+    ),
+  ).toBe(`Zdravo ${greetStrings.name}, vi radite kod ${greetStrings.company}`);
 
-    expect(youHaveNDollars.get({ amount: "1" }, { locale: "ba" })).toBe(
-        `Imate 1 dolar`
-    );
+  expect(youHaveNDollars.get({ amount: "1" }, { locale: "ba" })).toBe(
+    `Imate 1 dolar`,
+  );
 });
 
 test("Check variations", () => {
-    zykie.changeLocale("en");
+  zykie.changeLocale("en");
 
-    expect(variation.get({ variation: "fourth" })).toBe("default fourth");
-    expect(variation.get({ variation: "first" })).toBe("first first");
-    expect(variation.get({ variation: "second" })).toBe("second second");
+  expect(variation.get({ variation: "fourth" })).toBe("default fourth");
+  expect(variation.get({ variation: "first" })).toBe("first first");
+  expect(variation.get({ variation: "second" })).toBe("second second");
 
-    zykie.changeLocale("de");
-    expect(variation.get({ variation: "fourth" })).toBe("Standard fourth");
-    expect(variation.get({ variation: "first" })).toBe("erste first");
-    expect(variation.get({ variation: "second" })).toBe("zweite second");
+  zykie.changeLocale("de");
+  expect(variation.get({ variation: "fourth" })).toBe("Standard fourth");
+  expect(variation.get({ variation: "first" })).toBe("erste first");
+  expect(variation.get({ variation: "second" })).toBe("zweite second");
 
-    zykie.changeLocale("ba");
-    expect(variation.get({ variation: "fourth" })).toBe("Osnovni fourth");
-    expect(variation.get({ variation: "first" })).toBe("prvi first");
-    expect(variation.get({ variation: "second" })).toBe("drugi second");
+  zykie.changeLocale("ba");
+  expect(variation.get({ variation: "fourth" })).toBe("Osnovni fourth");
+  expect(variation.get({ variation: "first" })).toBe("prvi first");
+  expect(variation.get({ variation: "second" })).toBe("drugi second");
 
-    zykie.changeLocale("fr");
-    expect(variation.get({ variation: "fourth" })).toBe("Osnovni fourth");
-    expect(variation.get({ variation: "first" })).toBe("prvi first");
-    expect(variation.get({ variation: "second" })).toBe("drugi second");
+  zykie.changeLocale("fr");
+  expect(variation.get({ variation: "fourth" })).toBe("Osnovni fourth");
+  expect(variation.get({ variation: "first" })).toBe("prvi first");
+  expect(variation.get({ variation: "second" })).toBe("drugi second");
 
-    zykie.changeLocale("ba");
-    expect(
-        youHaveNDollars.get({
-            amount: "21",
-        })
-    ).toBe("Imate 21 dolar");
-    expect(
-        youHaveNDollars.get({
-            amount: "31",
-        })
-    ).toBe("Imate 31 dolar");
-    expect(
-        youHaveNDollars.get({
-            amount: "44",
-        })
-    ).toBe("Imate 44 dolara");
+  zykie.changeLocale("ba");
+  expect(
+    youHaveNDollars.get({
+      amount: "21",
+    }),
+  ).toBe("Imate 21 dolar");
+  expect(
+    youHaveNDollars.get({
+      amount: "31",
+    }),
+  ).toBe("Imate 31 dolar");
+  expect(
+    youHaveNDollars.get({
+      amount: "44",
+    }),
+  ).toBe("Imate 44 dolara");
 });
--- src/lib/zykie.ts
+++ src/lib/zykie.ts
@@ -1,172 +1,182 @@
-import {Condition, ConditionFunction, ConditionTranslations, GetVariablesFromString} from "./types";
+import {
+  Condition,
+  ConditionFunction,
+  ConditionTranslations,
+  GetVariablesFromString,
+} from "./types";
 
 let __currentLocale = "";
 
 export class Zykie<
-    TLocales extends readonly string[],
-    TFallbackLocale extends string,
+  TLocales extends readonly string[],
+  TFallbackLocale extends string,
 > {
-    private readonly fallbackLocale: TFallbackLocale;
+  private readonly fallbackLocale: TFallbackLocale;
 
-    constructor({
-        locales,
-        currentLocale,
-        fallbackLocale,
-    }: {
-        locales: TLocales;
-        currentLocale: TLocales[number];
-        fallbackLocale: TFallbackLocale;
-    }) {
-        if (!locales.includes(currentLocale)) {
-            throw new Error(
-                `Current locale "${currentLocale}" is not included in the list of locales`
-            );
+  constructor({
+    locales,
+    currentLocale,
+    fallbackLocale,
+  }: {
+    locales: TLocales;
+    currentLocale: TLocales[number];
+    fallbackLocale: TFallbackLocale;
+  }) {
+    if (!locales.includes(currentLocale)) {
+      throw new Error(
+        `Current locale "${currentLocale}" is not included in the list of locales`,
+      );
+    }
+
+    if (!locales.includes(fallbackLocale)) {
+      throw new Error(
+        `Default locale "${fallbackLocale}" is not included in the list of locales`,
+      );
+    }
+
+    __currentLocale = currentLocale;
+    this.fallbackLocale = fallbackLocale;
+  }
+
+  public create<TString extends string, TDefaultString extends string>(
+    translations: GetVariablesFromString<TString> extends GetVariablesFromString<TDefaultString>
+      ? {
+          [key in TFallbackLocale]: TDefaultString;
+        } & {
+          [key in Exclude<TLocales[number], TFallbackLocale>]: TString | null;
         }
+      : `The variables in the translations do not match the variables in the fallback '${TFallbackLocale}' translation`,
+  ) {
+    return new ZykieTranslation<TString, TDefaultString, TLocales>({
+      translations: translations as {
+        [key in TLocales[number]]: TString;
+      },
+      fallbackLocale: this.fallbackLocale,
+    });
+  }
 
-        if (!locales.includes(fallbackLocale)) {
-            throw new Error(
-                `Default locale "${fallbackLocale}" is not included in the list of locales`
-            );
-        }
-
-        __currentLocale = currentLocale;
-        this.fallbackLocale = fallbackLocale;
-    }
-
-    public create<TString extends string, TDefaultString extends string>(
-        translations: GetVariablesFromString<TString> extends GetVariablesFromString<TDefaultString>
-            ? {
-                  [key in TFallbackLocale]: TDefaultString;
-              } & {
-                  [key in Exclude<
-                      TLocales[number],
-                      TFallbackLocale
-                  >]: TString | null;
-              }
-            : `The variables in the translations do not match the variables in the fallback '${TFallbackLocale}' translation`
-    ) {
-
-        return new ZykieTranslation<TString, TDefaultString, TLocales>({
-            translations: translations as {
-                [key in TLocales[number]]: TString;
-            },
-            fallbackLocale: this.fallbackLocale,
-        });
-    }
-
-    public changeLocale(newLocale: TLocales[number]): void {
-        __currentLocale = newLocale;
-    }
+  public changeLocale(newLocale: TLocales[number]): void {
+    __currentLocale = newLocale;
+  }
 }
 
 class ZykieTranslation<
-    TString extends string,
-    TDefaultString extends string,
-    TLocales extends readonly string[],
+  TString extends string,
+  TDefaultString extends string,
+  TLocales extends readonly string[],
 > {
-    private readonly fallbackLocale: TLocales[number];
-    private conditions: Condition<TLocales, TString>[] = [];
+  private readonly fallbackLocale: TLocales[number];
+  private conditions: Condition<TLocales, TString>[] = [];
+  translations: {
+    [key in TLocales[number]]: TString;
+  };
+
+  constructor({
+    translations,
+    fallbackLocale,
+  }: {
     translations: {
-        [key in TLocales[number]]: TString;
+      [key in TLocales[number]]: TString;
     };
+    fallbackLocale: TLocales[number];
+  }) {
+    this.fallbackLocale = fallbackLocale;
+    this.translations = translations;
+  }
 
-    constructor({
-        translations,
-        fallbackLocale,
-    }: {
-        translations: {
-            [key in TLocales[number]]: TString;
-        };
-        fallbackLocale: TLocales[number];
-    }) {
-        this.fallbackLocale = fallbackLocale;
-        this.translations = translations;
-    }
-
-    get(
-        variables: {
-            [key in GetVariablesFromString<TString>[number]]: string;
-        },
-        options?: {
+  get(
+    ...args: GetVariablesFromString<TString>[number] extends never
+      ? [
+          options?: {
             locale?: TLocales[number];
-        }
-    ) {
-        const locale = options?.locale ?? __currentLocale;
-        let translatedString = this.getTranslatedStringByCondition(
-            locale,
-            variables
-        );
-
-        if (!translatedString) {
-            translatedString = this.getFallbackTranslatedString(locale);
-        }
-
-        return this.setVariables(translatedString, variables);
-    }
-
-    private getTranslatedStringByCondition(
-        locale: TLocales[number],
-        variables: {
+          },
+        ]
+      : [
+          variables: {
             [key in GetVariablesFromString<TString>[number]]: string;
-        }
-    ): string | null {
-        const condition = this.conditions.find((condition) =>
-            condition.function(variables)
-        );
-        let translatedString =
-            condition?.translations[locale] ??
-            this.translations[locale] ??
-            condition?.translations[this.fallbackLocale];
+          },
+          options?: {
+            locale?: TLocales[number];
+          },
+        ]
+  ) {
+    const [variables, options] = args;
+    const locale = options?.locale ?? __currentLocale;
+    let translatedString = this.getTranslatedStringByCondition(
+      locale,
+      variables,
+    );
 
-        return translatedString ?? null;
+    if (!translatedString) {
+      translatedString = this.getFallbackTranslatedString(locale);
     }
 
-    private getFallbackTranslatedString(locale: TLocales[number]) {
-        const translatedString = this.translations[this.fallbackLocale];
+    return this.setVariables(translatedString, variables);
+  }
 
-        if (!translatedString) {
-            throw new Error(
-                `No translation for locale "${locale}" found and no fallback locale provided`
-            );
-        }
+  private getTranslatedStringByCondition(
+    locale: TLocales[number],
+    variables: {
+      [key in GetVariablesFromString<TString>[number]]: string;
+    },
+  ): string | null {
+    const condition = this.conditions.find((condition) =>
+      condition.function(variables),
+    );
+    let translatedString =
+      condition?.translations[locale] ??
+      this.translations[locale] ??
+      condition?.translations[this.fallbackLocale];
 
-        console.warn(
-            `No translation for locale "${locale}" found, returning fallback locale translation: "${this.fallbackLocale}"`
-        );
+    return translatedString ?? null;
+  }
 
-        return translatedString;
+  private getFallbackTranslatedString(locale: TLocales[number]) {
+    const translatedString = this.translations[this.fallbackLocale];
+
+    if (!translatedString) {
+      throw new Error(
+        `No translation for locale "${locale}" found and no fallback locale provided`,
+      );
     }
 
-    private setVariables(
-        translatedString: string,
-        variables: {
-            [key in GetVariablesFromString<TString>[number]]: string;
-        }
-    ) {
-        const keys = Object.keys(variables) as (keyof typeof variables)[];
-        for (const key of keys) {
-            translatedString = translatedString.replace(
-                `var{${key}}`,
-                variables[key]
-            );
-        }
+    console.warn(
+      `No translation for locale "${locale}" found, returning fallback locale translation: "${this.fallbackLocale}"`,
+    );
 
-        return translatedString;
+    return translatedString;
+  }
+
+  private setVariables(
+    translatedString: string,
+    variables: {
+      [key in GetVariablesFromString<TString>[number]]: string;
+    },
+  ) {
+    const keys = Object.keys(variables) as (keyof typeof variables)[];
+    for (const key of keys) {
+      translatedString = translatedString.replace(
+        `var{${key}}`,
+        variables[key],
+      );
     }
 
-    variation<TVariation extends string>(
-        condition: ConditionFunction<TString>,
-        translations: Partial<{
-            [key in TLocales[number]]: GetVariablesFromString<TVariation> extends GetVariablesFromString<TString>
-                ? TVariation
-                : `The variables in the translations do not match the variables in the ${TDefaultString} translation`;
-        }>
-    ) {
-        this.conditions.push({
-            function: condition,
-            translations: translations as ConditionTranslations<TLocales>,
-        });
+    return translatedString;
+  }
 
-        return this;
-    }
+  variation<TVariation extends string>(
+    condition: ConditionFunction<TString>,
+    translations: Partial<{
+      [key in TLocales[number]]: GetVariablesFromString<TVariation> extends GetVariablesFromString<TString>
+        ? TVariation
+        : `The variables in the translations do not match the variables in the ${TDefaultString} translation`;
+    }>,
+  ) {
+    this.conditions.push({
+      function: condition,
+      translations: translations as ConditionTranslations<TLocales>,
+    });
+
+    return this;
+  }
 }
